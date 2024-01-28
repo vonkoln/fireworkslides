6b75d2d576f6d8d70e75f6d100c6101f2282c6f6
Bruno Stefano de Souza <40743118+vonkoln@users.noreply.github.com>
2024-01-28 15:44:53 -0300

    Update slide-stories.js

--- slide-stories.js
+++ slide-stories.js
@@ -44,7 +44,7 @@ class SlideStories {
 
   autoSlide() {
     clearTimeout(this.timeout);
-    this.timeout = setTimeout(this.next, 5000);
+    this.timeout = setTimeout(this.next, 15000);
   }
 
   init() {
